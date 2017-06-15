5e4030297a39085de0c8e374d9b4bb16a6774e80
hborcher <hborcher@illinois.edu>
2017-06-15 13:01:50 -0500

    added requirement

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='https://github.com/UIUCLibrary/ValidateMSI',
     license='',
     scripts=['validate_msi.py'],
+    install_requires=['PyYAML'],
     author='University of Illinois at Urbana Champaign',
     author_email='hborcher@illinois.edu',
     description='validate msi package'
