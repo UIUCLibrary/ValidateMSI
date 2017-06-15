f1cc03607188905d685e071651cf860586b55103
hborcher <hborcher@illinois.edu>
2017-06-15 12:59:21 -0500

    init commmit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Validate MSI
+============
+
+This script is used to make sure that msi packages contain all required files.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import validate_msi
+
+setup(
+    name='CheckRequiredFiles',
+    version=validate_msi.__version__,
+    url='https://github.com/UIUCLibrary/ValidateMSI',
+    license='',
+    scripts=['validate_msi.py'],
+    author='University of Illinois at Urbana Champaign',
+    author_email='hborcher@illinois.edu',
+    description='validate msi package'
+)
